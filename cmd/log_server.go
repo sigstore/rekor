915825262f77986978d8da7dc730d824de2f06df
Luke Hinds <lhinds@redhat.com>
2020-09-01 13:02:57 +0100

    Basic inclusion command for GetInclusionProofByHash

--- cmd/log_server.go
+++ cmd/log_server.go
@@ -19,20 +19,32 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"github.com/google/trillian"
-	"github.com/google/trillian/merkle/rfc6962"
-	"google.golang.org/grpc/codes"
 	"log"
+	"time"
+
+	"github.com/google/trillian"
+	"github.com/google/trillian/client"
+	"github.com/google/trillian/merkle"
+	"github.com/google/trillian/merkle/rfc6962"
+	"github.com/google/trillian/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type server struct {
+	lc     *client.LogClient
 	client trillian.TrillianLogClient
 	logID  int64
 	ctx    context.Context
 }
 
 type Response struct {
-	status string
+	status   string
+	leafhash string
+}
+
+type InclusionResponse struct {
+	status   string
 	leafhash string
 }
 
@@ -41,6 +53,70 @@ func serverInstance(client trillian.TrillianLogClient, tLogID int64) *server {
 		client: client,
 		logID:  tLogID,
 	}
+}
+
+func (s *server) root() (types.LogRootV1, error) {
+	rqst := &trillian.GetLatestSignedLogRootRequest{
+		LogId: s.logID,
+	}
+	resp, err := s.client.GetLatestSignedLogRoot(context.Background(), rqst)
+	if err != nil {
+		return types.LogRootV1{}, err
+	}
+	var root types.LogRootV1
+	if err := root.UnmarshalBinary(resp.SignedLogRoot.LogRoot); err != nil {
+		return types.LogRootV1{}, err
+	}
+	return root, nil
+}
+
+func (s *server) getInclusion(byteValue []byte, tLogID int64) (*Response, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	root, err := s.root()
+	if err != nil {
+		return &Response{}, err
+	}
+
+	log.Printf("[server:wait] Root hash: %x", root.RootHash)
+
+	hasher := rfc6962.DefaultHasher
+	leafHash := hasher.HashLeaf(byteValue)
+
+	resp, err := s.client.GetInclusionProofByHash(ctx,
+		&trillian.GetInclusionProofByHashRequest{
+			LogId:    tLogID,
+			LeafHash: leafHash,
+			TreeSize: int64(root.TreeSize),
+		})
+	if err != nil {
+		fmt.Println("one")
+		return nil, status.Errorf(codes.Internal, "failed to get inclusion proof: %v", err)
+	}
+	if err != nil {
+		fmt.Println("two")
+		return &Response{}, err
+	}
+	if len(resp.Proof) < 1 {
+		fmt.Println("three")
+		return &Response{}, nil
+	}
+
+	v := merkle.NewLogVerifier(rfc6962.DefaultHasher)
+	for i, proof := range resp.Proof {
+		hashes := proof.GetHashes()
+		for j, hash := range hashes {
+			log.Printf("Proof[%d],hash[%d] == %x\n", i, j, hash)
+		}
+		if err := v.VerifyInclusionProof(proof.LeafIndex, int64(root.TreeSize), hashes, root.RootHash, leafHash); err != nil {
+			return &Response{}, err
+		}
+	}
+	return &Response{
+		status: "ok",
+	}, nil
 }
 
 func (s *server) addLeaf(byteValue []byte, tLogID int64) (*Response, error) {
