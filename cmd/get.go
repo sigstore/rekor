c75bdfb1248179bb6934d8ba77c77e0c025f496f
dlorenc <dlorenc@google.com>
2020-10-27 05:34:01 -0500

    Verify command. (#27)
    Seems to work for Node right now. I haven't tested on linux.

--- cmd/get.go
+++ cmd/get.go
@@ -16,21 +16,9 @@ limitations under the License.
 package cmd
 
 import (
-	"context"
-	"crypto"
-	"crypto/x509"
-	"encoding/json"
 	"io/ioutil"
-	"net/http"
-	"time"
-
-	tcrypto "github.com/google/trillian/crypto"
-
-	tclient "github.com/google/trillian/client"
 
 	"github.com/google/trillian"
-	"github.com/google/trillian/merkle"
-	"github.com/google/trillian/merkle/rfc6962"
 	"github.com/projectrekor/rekor-cli/log"
 	"github.com/spf13/viper"
 
@@ -56,64 +44,11 @@ exists within the transparency log`,
 		url := rekorServer + "/api/v1/getproof"
 		rekord := viper.GetString("rekord")
 
-		// Set Context with Timeout for connects to thde log rpc server
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		request, err := http.NewRequestWithContext(ctx, "POST", url, nil)
+		rekorEntry, err := ioutil.ReadFile(rekord)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		if err := addFileToRequest(request, rekord); err != nil {
-			log.Fatal(err)
-		}
-
-		client := &http.Client{}
-		response, err := client.Do(request)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer response.Body.Close()
-
-		content, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		resp := getProofResponse{}
-		if err := json.Unmarshal(content, &resp); err != nil {
-			log.Fatal(err)
-		}
-
-		pub, err := x509.ParsePKIXPublicKey(resp.Key)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		f, err := ioutil.ReadFile(rekord)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if resp.Proof != nil {
-			leafHash := rfc6962.DefaultHasher.HashLeaf(f)
-			verifier := tclient.NewLogVerifier(rfc6962.DefaultHasher, pub, crypto.SHA256)
-			root, err := tcrypto.VerifySignedLogRoot(verifier.PubKey, verifier.SigHash, resp.Proof.SignedLogRoot)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			v := merkle.NewLogVerifier(rfc6962.DefaultHasher)
-			proof := resp.Proof.Proof[0]
-			if err := v.VerifyInclusionProof(proof.LeafIndex, int64(root.TreeSize), proof.Hashes, root.RootHash, leafHash); err != nil {
-				log.Fatal(err)
-			}
-			log.Info("Proof correct!")
-		} else {
-			log.Info(resp.Status)
-		}
+		DoGet(url, rekorEntry)
 	},
 }
 
--- go.mod
+++ go.mod
@@ -3,12 +3,17 @@ module github.com/projectrekor/rekor-cli
 go 1.14
 
 require (
-	github.com/google/trillian v1.3.8
+	github.com/coreos/bbolt v1.3.3 // indirect
+	github.com/coreos/etcd v3.3.18+incompatible // indirect
+	github.com/google/certificate-transparency-go v1.1.0 // indirect
+	github.com/google/trillian v1.3.10
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/sirupsen/logrus v1.4.2
+	github.com/projectrekor/rekor-server v0.0.0-20201020185212-eabecb525492
+	github.com/prometheus/common v0.10.0
 	github.com/spf13/cobra v1.0.0
-	github.com/spf13/viper v1.7.0
-	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816
-	go.uber.org/zap v1.13.0
-	google.golang.org/grpc v1.25.1
+	github.com/spf13/viper v1.7.1
+	go.etcd.io/etcd v3.3.25+incompatible // indirect
+	go.uber.org/zap v1.16.0
 )
+
+replace google.golang.org/grpc => google.golang.org/grpc v1.29.1
