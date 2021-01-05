0a6001d9f44f10d6cdd5d4866219c9a8b3c12b40
Dan Lorenc <dlorenc@google.com>
2021-01-05 07:08:30 -0600

    Rewrite the e2e tests in go from bash.

--- tests/e2e-test.sh
+++ tests/e2e-test.sh
@@ -4,21 +4,6 @@ testdir=$(dirname "$0")
 
 docker-compose up -d
 
-# Node
-nodedir=${testdir}/node
+go build -o rekor-cli ./cmd/cli
 
-# First time we should get "Created entry"
-out=$(go run ./cmd/cli/ upload \
-    --artifact $(< ${nodedir}/url) --sha $(< ${nodedir}/sha) \
-    --signature=${nodedir}/sig  --public-key=${nodedir}/key)
-if [[ $out != *"Created entry at"* ]]; then
-    echo "Expected 'Created entry at', got $out"
-fi
-
-# Second time we should get "Entry already exists"
-out=$(go run ./cmd/cli/ upload \
-    --artifact $(< ${nodedir}/url) --sha $(< ${nodedir}/sha) \
-    --signature=${nodedir}/sig  --public-key=${nodedir}/key)
-if [[ $out != *"Entry already exists"* ]]; then
-    echo "Expected 'Created entry at', got $out"
-fi
+go test -tags=e2e ./tests/
