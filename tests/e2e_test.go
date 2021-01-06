562e91bac279c41b55c8f2305a47d0067729690b
Dan Lorenc <dlorenc@google.com>
2021-01-06 09:55:55 -0600

    Change the e2e tests to generate new, random data.
    This way we don't have to worry about existing entries in the logs.

--- tests/e2e_test.go
+++ tests/e2e_test.go
@@ -3,10 +3,14 @@
 package e2e
 
 import (
+	"encoding/base64"
 	"io/ioutil"
+	"math/rand"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 )
 
 const (
@@ -41,20 +45,57 @@ func readFile(t *testing.T, p string) string {
 	return strings.TrimSpace(string(b))
 }
 
+// createdSignedArtifact gets the test dir setup correctly with some random artifacts and keys.
+func createdSignedArtifact(t *testing.T, artifactPath, sigPath string) {
+	t.Helper()
+	// First let's generate some random data so we don't have to worry about dupes.
+	rand.Seed(time.Now().UnixNano())
+	data := [100]byte{}
+	if _, err := rand.Read(data[:]); err != nil {
+		t.Fatal(err)
+	}
+
+	artifact := base64.StdEncoding.EncodeToString(data[:])
+	// Write this to a file
+	if err := ioutil.WriteFile(artifactPath, []byte(artifact), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	// Sign it with our key and write that to a file
+	signature := Sign(t, strings.NewReader(artifact))
+	if err := ioutil.WriteFile(sigPath, []byte(signature), 0644); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestDuplicates(t *testing.T) {
-	// Start with uploading the node data once
-	out := runCli(t, "upload", "--artifact", readFile(t, "node/url"), "--sha", readFile(t, "node/sha"),
-		"--signature=node/sig", "--public-key=node/key")
+	artifactPath := filepath.Join(t.TempDir(), "artifact")
+	sigPath := filepath.Join(t.TempDir(), "signature.asc")
+
+	createdSignedArtifact(t, artifactPath, sigPath)
+
+	// Write the public key to a file
+	pubPath := filepath.Join(t.TempDir(), "pubKey.asc")
+	if err := ioutil.WriteFile(pubPath, []byte(publicKey), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	// Now upload to rekor!
+	out := runCli(t, "upload", "--artifact", artifactPath, "--signature", sigPath, "--public-key", pubPath)
 	if !strings.Contains(out, "Created entry at") {
 		t.Errorf("Expected [Created entry at], got %s", out)
 	}
-	t.Log(out)
 
-	// Now do it again, there should be a duplicate message
-	out = runCli(t, "upload", "--artifact", readFile(t, "node/url"), "--sha", readFile(t, "node/sha"),
-		"--signature=node/sig", "--public-key=node/key")
+	// Now upload the same one again, we should get a dupe entry.
+	out = runCli(t, "upload", "--artifact", artifactPath, "--signature", sigPath, "--public-key", pubPath)
 	if !strings.Contains(out, "Entry already exists") {
 		t.Errorf("Expected [Entry already exists], got %s", out)
 	}
-	t.Log(out)
+
+	// Now do a new one, we should get a new entry
+	createdSignedArtifact(t, artifactPath, sigPath)
+	out = runCli(t, "upload", "--artifact", artifactPath, "--signature", sigPath, "--public-key", pubPath)
+	if !strings.Contains(out, "Created entry at") {
+		t.Errorf("Expected [Created entry at], got %s", out)
+	}
 }
