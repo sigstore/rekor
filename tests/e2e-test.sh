8a198ed148781d37412ba2d9e52a65c4d41a3b85
Bob Callaway <bcallawa@redhat.com>
2021-03-03 09:25:19 -0500

    fix race condition in e2e tests
    In our CI environment there is an artifical delay in between starting
    the Rekor services via docker-compose and when the E2E tests are
    actually executed due to Go modules being downloaded. In a local
    development environment, the download may not be required so the tests
    can start before the docker-compose services are actually running.
    
    This introduces a healthcheck for services (where possible), and blocks
    the start of the e2e tests until the services are reporting as healthy.
    It also forces the use of an empty homedir and rekor config file to
    ensure no collision between the tests and the developer's environment.
    
    Fixes #183
    
    Signed-off-by: Bob Callaway <bcallawa@redhat.com>

--- tests/e2e-test.sh
+++ tests/e2e-test.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
-set -ex
+#set -ex
 testdir=$(dirname "$0")
 
+echo "starting services"
 docker-compose up -d
 
+echo "building CLI"
 go build -o rekor-cli ./cmd/cli
 
-go test -tags=e2e ./tests/
+count=0
+
+echo -n "waiting up to 60 sec for system to start"
+until [ $(docker-compose ps | grep -c "(healthy)") == 3 ];
+do
+    if [ $count -eq 6 ]; then
+       echo "! timeout reached"
+       exit 1
+    else
+       echo -n "."
+       sleep 10
+       let 'count+=1'
+    fi
+done
+
+echo
+echo "running tests"
+TMPDIR="$(mktemp -d -t rekor_test)"
+touch $TMPDIR.rekor.yaml
+trap "rm -rf $TMPDIR" EXIT
+TMPDIR=$TMPDIR go test -tags=e2e ./tests/
