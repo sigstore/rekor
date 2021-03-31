417ffe5323356a28cf3bedb4c7d13cc103e49fa7
Ahmet Alp Balkan <ahmetb@google.com>
2021-03-31 09:28:43 -0700

    rekor-cli: logproof -last-size default fix
    -last-size is defaulting to 1, but fixing it to 0 so we can properly error out
    if it's unspecified.
    
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- cmd/rekor-cli/app/log_proof.go
+++ cmd/rekor-cli/app/log_proof.go
@@ -94,7 +94,7 @@ var logProofCmd = &cobra.Command{
 
 func init() {
 	logProofCmd.Flags().Uint64("first-size", 1, "the size of the log where the proof should begin")
-	logProofCmd.Flags().Uint64("last-size", 1, "the size of the log where the proof should end")
+	logProofCmd.Flags().Uint64("last-size", 0, "the size of the log where the proof should end")
 	if err := logProofCmd.MarkFlagRequired("last-size"); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
