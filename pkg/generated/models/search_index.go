53072432e7963a45396f66ce4c6beb6bbaec05d8
Mikhail Swift <mswift@mswift.dev>
2021-11-17 09:57:04 -0500

    api/index/retrieve: allow searching on indicies with sha1 hashes (#499)
    Adds the ability to search for indicies with sha1 hashes. Currently
    rekor custom types can store indices with formats other than
    sha256:<hash>.  Particularly the in-toto type can do this.
    
    One particular use case of interest is indexing log entries by git
    commit hash, which largely still use sha1.
    
    Signed-off-by: Mikhail Swift <mswift@mswift.dev>

--- pkg/generated/models/search_index.go
+++ pkg/generated/models/search_index.go
@@ -41,7 +41,7 @@ type SearchIndex struct {
 	Email strfmt.Email `json:"email,omitempty"`
 
 	// hash
-	// Pattern: ^(sha256:)?[0-9a-fA-F]{64}$
+	// Pattern: ^(sha256:)?[0-9a-fA-F]{64}$|^(sha1:)?[0-9a-fA-F]{40}$
 	Hash string `json:"hash,omitempty"`
 
 	// public key
@@ -87,7 +87,7 @@ func (m *SearchIndex) validateHash(formats strfmt.Registry) error {
 		return nil
 	}
 
-	if err := validate.Pattern("hash", "body", m.Hash, `^(sha256:)?[0-9a-fA-F]{64}$`); err != nil {
+	if err := validate.Pattern("hash", "body", m.Hash, `^(sha256:)?[0-9a-fA-F]{64}$|^(sha1:)?[0-9a-fA-F]{40}$`); err != nil {
 		return err
 	}
 
