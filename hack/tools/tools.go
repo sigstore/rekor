47d5325230715cb01de0a8814a909cc11546859d
dlorenc <dlorenc@google.com>
2021-12-20 07:21:15 -0600

    Switch the trillian containers to multi-plat builds. (#547)
    This also adds the ko targets for building them, and entries
    to tools.go so go.mod recognizes these dependencies so we can build
    them with ko.
    
    Signed-off-by: Dan Lorenc <lorenc.d@gmail.com>

--- hack/tools/tools.go
+++ hack/tools/tools.go
@@ -1,3 +1,4 @@
+//go:build tools
 // +build tools
 
 // Copyright 2021 The Sigstore Authors.
@@ -24,4 +25,8 @@ import (
 	_ "github.com/dvyukov/go-fuzz/go-fuzz-build"
 	_ "github.com/dvyukov/go-fuzz/go-fuzz-dep"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
+
+	// These are so we can build these two binaries into containers with ko
+	_ "github.com/google/trillian/cmd/trillian_log_server"
+	_ "github.com/google/trillian/cmd/trillian_log_signer"
 )
