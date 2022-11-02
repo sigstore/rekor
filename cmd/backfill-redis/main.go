5c8d2776737b6c1b254e393226644fda6200903c
Hayden Blauzvern <hblauzvern@google.com>
2022-11-02 23:04:30 +0000

    Fix bugs in script
    Signed-off-by: Hayden Blauzvern <hblauzvern@google.com>

--- cmd/backfill-redis/main.go
+++ cmd/backfill-redis/main.go
@@ -25,10 +25,22 @@ import (
 	"github.com/go-openapi/runtime"
 	radix "github.com/mediocregopher/radix/v4"
 	"github.com/sigstore/rekor/pkg/client"
-	"github.com/sigstore/rekor/pkg/types"
-
 	"github.com/sigstore/rekor/pkg/generated/client/entries"
 	"github.com/sigstore/rekor/pkg/generated/models"
+	"github.com/sigstore/rekor/pkg/types"
+
+	// these imports are to call the packages' init methods
+	_ "github.com/sigstore/rekor/pkg/types/alpine/v0.0.1"
+	_ "github.com/sigstore/rekor/pkg/types/cose/v0.0.1"
+	_ "github.com/sigstore/rekor/pkg/types/hashedrekord/v0.0.1"
+	_ "github.com/sigstore/rekor/pkg/types/helm/v0.0.1"
+	_ "github.com/sigstore/rekor/pkg/types/intoto/v0.0.1"
+	_ "github.com/sigstore/rekor/pkg/types/intoto/v0.0.2"
+	_ "github.com/sigstore/rekor/pkg/types/jar/v0.0.1"
+	_ "github.com/sigstore/rekor/pkg/types/rekord/v0.0.1"
+	_ "github.com/sigstore/rekor/pkg/types/rfc3161/v0.0.1"
+	_ "github.com/sigstore/rekor/pkg/types/rpm/v0.0.1"
+	_ "github.com/sigstore/rekor/pkg/types/tuf/v0.0.1"
 )
 
 var (
@@ -64,38 +76,46 @@ func main() {
 		log.Fatal(err)
 	}
 
-	rekorClient, err := client.GetRekorClient("https://rekor.sigstore.dev")
+	rekorClient, err := client.GetRekorClient(*rekorAddress)
 	if err != nil {
 		log.Fatalf("creating rekor client: %v", err)
 	}
 
 	for i := *startIndex; i <= *endIndex; i++ {
 		params := entries.NewGetLogEntryByIndexParamsWithContext(context.Background())
-		params.SetLogIndex(int64(*startIndex))
+		params.SetLogIndex(int64(i))
 		resp, err := rekorClient.Entries.GetLogEntryByIndex(params)
 		if err != nil {
 			log.Fatalf("retrieving log uuid by index: %v", err)
 		}
+		success := true
 		for uuid, entry := range resp.Payload {
 			// uuid is the global UUID - tree ID and entry UUID
 			e, _, _, err := unmarshalEntryImpl(entry.Body.(string))
 			if err != nil {
 				fmt.Printf("error unmarshalling entry for %s: %v\n", uuid, err)
+				success = false
 				continue
 			}
 			keys, err := e.IndexKeys()
 			if err != nil {
 				fmt.Printf("error building index keys for %s: %v\n", uuid, err)
+				success = false
 				continue
 			}
 			for _, key := range keys {
 				if err := addToIndex(context.Background(), redisClient, key, uuid); err != nil {
+					success = false
 					fmt.Printf("error inserting UUID %s with key %s: %v\n", uuid, key, err)
 				}
 				fmt.Printf("Uploaded Redis entry %s, index %d, key %s\n", uuid, i, key)
 			}
 		}
-		fmt.Printf("Completed log index %d\n", i)
+		if success {
+			fmt.Printf("Completed log index %d\n", i)
+		} else {
+			fmt.Printf("Errors with log index %d\n", i)
+		}
 	}
 }
 
