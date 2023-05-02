46ac0b224e673b969457597b15af2d125ae8c5c2
Hayden B <hblauzvern@google.com>
2023-05-02 12:20:19 -0700

    Refactor Trillian client with exported methods (#1454)
    This allows the Trillian client to be used in other parts of the
    codebase besides the api package. The changes included exporting the
    Response fields and all but one of the struct methods. Also removed
    ranges since these weren't used outside the api package.
    
    Signed-off-by: Hayden Blauzvern <hblauzvern@google.com>

--- pkg/api/public_key.go
+++ pkg/api/public_key.go
@@ -26,10 +26,8 @@ import (
 )
 
 func GetPublicKeyHandler(params pubkey.GetPublicKeyParams) middleware.Responder {
-	ctx := params.HTTPRequest.Context()
 	treeID := swag.StringValue(params.TreeID)
-	tc := NewTrillianClient(ctx)
-	pk, err := tc.ranges.PublicKey(api.pubkey, treeID)
+	pk, err := api.logRanges.PublicKey(api.pubkey, treeID)
 	if err != nil {
 		return handleRekorAPIError(params, http.StatusBadRequest, err, "")
 	}
