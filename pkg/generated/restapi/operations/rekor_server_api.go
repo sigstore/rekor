2ba7cc8cba107dcc94cbdb2972a38b273e2623b4
Bob Callaway <bobcallaway@users.noreply.github.com>
2023-06-12 20:39:21 -0400

    bump go-swagger to v0.30.5 (#1532)
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- pkg/generated/restapi/operations/rekor_server_api.go
+++ pkg/generated/restapi/operations/rekor_server_api.go
@@ -413,6 +413,6 @@ func (o *RekorServerAPI) AddMiddlewareFor(method, path string, builder middlewar
 	}
 	o.Init()
 	if h, ok := o.handlers[um][path]; ok {
-		o.handlers[method][path] = builder(h)
+		o.handlers[um][path] = builder(h)
 	}
 }
