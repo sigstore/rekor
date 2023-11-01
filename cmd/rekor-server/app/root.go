12d546cf817cbd11b7d488a4d5a793a988b11733
Hayden B <hblauzvern@google.com>
2023-11-01 10:47:44 -0700

    Update signer flag description (#1804)
    Mentions support for all KMS providers
    
    Signed-off-by: Hayden B <hblauzvern@google.com>

--- cmd/rekor-server/app/root.go
+++ cmd/rekor-server/app/root.go
@@ -97,7 +97,7 @@ func init() {
 	rootCmd.PersistentFlags().String("rekor_server.address", "127.0.0.1", "Address to bind to")
 
 	rootCmd.PersistentFlags().String("rekor_server.signer", "memory",
-		`Rekor signer to use. Valid options are: [gcpkms, memory, filename containing PEM encoded private key].
+		`Rekor signer to use. Valid options are: [awskms://keyname, azurekms://keyname, gcpkms://keyname, hashivault://keyname, memory, <filename containing PEM-encoded private key>].
 Memory and file-based signers should only be used for testing.`)
 	rootCmd.PersistentFlags().String("rekor_server.signer-passwd", "", "Password to decrypt signer private key")
 
