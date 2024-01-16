f28ea3a6c71836b5a22f0c3967e6c21b1bc74814
Riccardo Schirone <riccardo.schirone@trailofbits.com>
2024-01-16 14:02:06 +0100

    Added support for sha384/sha512 hash algorithms in hashedrekords
    Includes changes provided by @bobcallaway
    
    Signed-off-by: Riccardo Schirone <riccardo.schirone@trailofbits.com>

--- pkg/util/sha_test.go
+++ pkg/util/sha_test.go
@@ -15,6 +15,7 @@
 package util
 
 import (
+	"crypto"
 	"testing"
 )
 
@@ -43,12 +44,78 @@ func TestPrefixSHA(t *testing.T) {
 			input: "cfd356237e261871e8f92ae6710a75a65a925ae121d94d28533f008bd3e00b5472d261b5d0e1ab4082e3078dd1ad2af57876ed3c1c797c4097dbed870f458408",
 			want:  "sha512:cfd356237e261871e8f92ae6710a75a65a925ae121d94d28533f008bd3e00b5472d261b5d0e1ab4082e3078dd1ad2af57876ed3c1c797c4097dbed870f458408",
 		},
+		{
+			input: "78674b244bc9cba8ecb6dcb660b059728236e36b2f30fbcd6e17b1b64255f3ac596fbe5c84d1cc9d2a0979513260de09",
+			want:  "sha384:78674b244bc9cba8ecb6dcb660b059728236e36b2f30fbcd6e17b1b64255f3ac596fbe5c84d1cc9d2a0979513260de09",
+		},
 	}
 
 	for _, tr := range testCases {
 		got := PrefixSHA(tr.input)
 		if got != tr.want {
 			t.Errorf("Got '%s' expected '%s'", got, tr.want)
+		}
+	}
+}
+
+func TestUnprefixSHA(t *testing.T) {
+	type prefixedSHA struct {
+		crypto.Hash
+		string
+	}
+	var testCases = []struct {
+		input string
+		want  prefixedSHA
+	}{
+		{
+			input: "87428fc522803d31065e7bce3cf03fe475096631e5e07bbd7a0fde60c4cf25c7",
+			want: prefixedSHA{
+				crypto.SHA256,
+				"87428fc522803d31065e7bce3cf03fe475096631e5e07bbd7a0fde60c4cf25c7",
+			},
+		},
+		{
+			input: "sha512:162b0b32f02482d5aca0a7c93dd03ceac3acd7e410a5f18f3fb990fc958ae0df6f32233b91831eaf99ca581a8c4ddf9c8ba315ac482db6d4ea01cc7884a635be",
+			want: prefixedSHA{
+				crypto.SHA512,
+				"162b0b32f02482d5aca0a7c93dd03ceac3acd7e410a5f18f3fb990fc958ae0df6f32233b91831eaf99ca581a8c4ddf9c8ba315ac482db6d4ea01cc7884a635be",
+			},
+		},
+		{
+			input: "09b80428c53912d4174162fd5b7c7d485bdcc3ab",
+			want: prefixedSHA{
+				crypto.SHA1,
+				"09b80428c53912d4174162fd5b7c7d485bdcc3ab",
+			},
+		},
+		{
+			input: "cfd356237e261871e8f92ae6710a75a65a925ae121d94d28533f008bd3e00b5472d261b5d0e1ab4082e3078dd1ad2af57876ed3c1c797c4097dbed870f458408",
+			want: prefixedSHA{
+				crypto.SHA512,
+				"cfd356237e261871e8f92ae6710a75a65a925ae121d94d28533f008bd3e00b5472d261b5d0e1ab4082e3078dd1ad2af57876ed3c1c797c4097dbed870f458408",
+			},
+		},
+		{
+			input: "78674b244bc9cba8ecb6dcb660b059728236e36b2f30fbcd6e17b1b64255f3ac596fbe5c84d1cc9d2a0979513260de09",
+			want: prefixedSHA{
+				crypto.SHA384,
+				"78674b244bc9cba8ecb6dcb660b059728236e36b2f30fbcd6e17b1b64255f3ac596fbe5c84d1cc9d2a0979513260de09",
+			},
+		},
+		{
+			input: "sha384:78674b244bc9cba8ecb6dcb660b059728236e36b2f30fbcd6e17b1b64255f3ac596fbe5c84d1cc9d2a0979513260de09",
+			want: prefixedSHA{
+				crypto.SHA384,
+				"78674b244bc9cba8ecb6dcb660b059728236e36b2f30fbcd6e17b1b64255f3ac596fbe5c84d1cc9d2a0979513260de09",
+			},
+		},
+	}
+
+	for _, tr := range testCases {
+		algo, value := UnprefixSHA(tr.input)
+		got := prefixedSHA{algo, value}
+		if got != tr.want {
+			t.Errorf("Got '%v' expected '%v' (input %s)", got, tr.want, tr.input)
 		}
 	}
 }
