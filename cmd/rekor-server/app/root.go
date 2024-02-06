d50deec0996eaff908b9032257cc822315655e18
Mihkel Pärna <mihkel.parna@transferwise.com>
2024-02-06 14:16:17 +0200

    Define 'redis_server.insecure-skip-verify' flag in 'rekor-server' to allow skipping TLS verification
    Signed-off-by: Mihkel Pärna <mihkel.parna@transferwise.com>

--- cmd/rekor-server/app/root.go
+++ cmd/rekor-server/app/root.go
@@ -115,6 +115,7 @@ Memory and file-based signers should only be used for testing.`)
 	rootCmd.PersistentFlags().String("redis_server.address", "127.0.0.1", "Redis server address")
 	rootCmd.PersistentFlags().Uint16("redis_server.port", 6379, "Redis server port")
 	rootCmd.PersistentFlags().String("redis_server.password", "", "Redis server password")
+	rootCmd.PersistentFlags().Bool("redis_server.insecure-skip-verify", false, "Whether to skip TLS verification or not")
 
 	rootCmd.PersistentFlags().Bool("enable_attestation_storage", false, "enables rich attestation storage")
 	rootCmd.PersistentFlags().String("attestation_storage_bucket", "", "url for attestation storage bucket")
