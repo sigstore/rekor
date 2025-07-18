9b4c282d12ba733723fde2ef7c7a854c4df0f0cc
Bob Callaway <bcallaway@google.com>
2025-07-17 18:48:38 -0700

    reuse var
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -184,7 +184,7 @@ func NewAPI(treeID uint) (*API, error) {
 	cachedCheckpoints := make(map[int64]string)
 	for _, r := range ranges.GetInactive() {
 		tc := trillianclient.NewTrillianClient(logClient, r.TreeID)
-		resp := tc.GetLatest(context.Background(), 0)
+		resp := tc.GetLatest(ctx, 0)
 		if resp.Status != codes.OK {
 			return nil, fmt.Errorf("error fetching latest tree head for inactive shard %d: resp code is %d, err is %w", r.TreeID, resp.Status, resp.Err)
 		}
