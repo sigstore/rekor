10d39e97f333660a1d8372b00e7d9116ec0be8e1
Bob Callaway <bcallaway@google.com>
2026-08-10 08:57:28 -0400

    better handle connection issues
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- pkg/api/error_test.go
+++ pkg/api/error_test.go
@@ -24,7 +24,28 @@ import (
 
 	"github.com/go-openapi/runtime"
 	"github.com/sigstore/rekor/pkg/generated/restapi/operations/entries"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+func TestMapGRPCToHTTP(t *testing.T) {
+	tests := []struct {
+		code int
+		err  error
+		want int
+	}{
+		{http.StatusOK, status.Error(codes.Canceled, "context canceled"), http.StatusOK},
+		{http.StatusInternalServerError, status.Error(codes.Canceled, "context canceled"), 499},
+		{http.StatusInternalServerError, status.Error(codes.DeadlineExceeded, "deadline exceeded"), http.StatusGatewayTimeout},
+		{http.StatusInternalServerError, status.Error(codes.DataLoss, "dataloss"), http.StatusInternalServerError},
+	}
+
+	for _, tt := range tests {
+		if got := mapGRPCToHTTP(tt.code, tt.err); got != tt.want {
+			t.Errorf("mapGRPCToHTTP(%v, %v) = %v, want %v", tt.code, tt.err, got, tt.want)
+		}
+	}
+}
 
 func TestHandleRekorAPIError_ClientCanceled(t *testing.T) {
 	// Case 1: When client context is NOT canceled, we expect a standard HTTP 500 response.
