d8bd80452e41ae5be1d792f391531ff742e8246b
Bob Callaway <2845093+bobcallaway@users.noreply.github.com>
2026-08-24 14:06:57 -0400

    dsse parsing cpu/memory optimization (#2946)
    * dsse memory optimizations
    
    Signed-off-by: Bob Callaway <bcallaway@google.com>
    
    * add comment for old definitions
    
    Signed-off-by: Bob Callaway <bcallaway@google.com>
    
    ---------
    
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- pkg/types/dsse/v0.0.1/benchmark_test.go
+++ pkg/types/dsse/v0.0.1/benchmark_test.go
@@ -297,6 +297,19 @@ func benchmarkUnmarshalBefore(v *V001Entry, pe models.ProposedEntry) error {
 		})
 	}
 
+	// old index key extraction struct defs pre #2946
+	type indexKeyExtract struct {
+		Subject []struct {
+			Digest map[string]string `json:"digest"`
+		} `json:"subject"`
+		Predicate json.RawMessage `json:"predicate"`
+	}
+	type materialsExtract struct {
+		Materials []struct {
+			Digest map[string]string `json:"digest"`
+		} `json:"materials"`
+	}
+
 	if env.PayloadType == in_toto.PayloadType {
 		var extract indexKeyExtract
 		if err := json.Unmarshal(decodedPayload, &extract); err == nil {
